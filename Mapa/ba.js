b88a693659268fc055654fa0de628f8eb148e938
feliann <101577329+feliann@users.noreply.github.com>
2022-08-28 21:59:11 -0300

    geojson puesto

--- Mapa/ba.js
+++ Mapa/ba.js
@@ -1,6 +1,7 @@
 // Maps access token goes here
 var key = 'pk.bac3cbaedf6a01fdff654f7b86f1e863';
 
+//Agregar Mapa
 let map = L.map('map').setView([-34.612918138033976, -58.43811578328029],12.3)
  
 L.tileLayer('https://maps.googleapis.com/maps/vt?pb=!1m5!1m4!1i{z}!2i{x}!3i{y}!4i256!2m3!1e0!2sm!3i349018013!3m9!2sen-US!3sUS!5e18!12m1!1e47!12m3!1e37!2m1!1ssmartmaps!4e0',{
@@ -8,7 +9,8 @@ L.tileLayer('https://maps.googleapis.com/maps/vt?pb=!1m5!1m4!1i{z}!2i{x}!3i{y}!4
 }).addTo(map);
 
 
-// Add geocoder
+
+// Agregar geocoder
  L.control.geocoder(key, {
   fullWidth: 650,
   expanded: true,
@@ -16,6 +18,21 @@ L.tileLayer('https://maps.googleapis.com/maps/vt?pb=!1m5!1m4!1i{z}!2i{x}!3i{y}!4
   
 }).addTo(map);
 
+// Configurar PopUp
+function popup(feature,layer){
+  if(feature.properties && feature.properties.BARRIO){
+      layer.bindPopup("<strong>Barrio: </strong>" + feature.properties.BARRIO + "<br/>");
+     
+  }
+}
 
+// Agregar capa en formato GeoJson
+L.geoJson(garage).addTo(map);
+
+var garage = L.geoJson(garage,{
+  onEachFeature: popup
+}).addTo(map);
+
+//Agregar Localizador
 L.control.locate().addTo(map);
 
