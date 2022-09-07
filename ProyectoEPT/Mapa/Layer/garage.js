ff363b367d8fa64af17826a9afbc8b497530101b
lucianojacofsky <101577485+lucianojacofsky@users.noreply.github.com>
2022-09-07 15:16:19 -0300

    ORDENAMOS EL GIT
    Borramos todo y lo dividimos por carpetas todo

---
+++ ProyectoEPT/Mapa/Layer/garage.js
@@ -0,0 +1,19 @@
+var garage =
+{
+"type": "FeatureCollection",
+"name": "garages",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "SMP": "047-106-028", "CALLE": "ARENAL, CONCEPCION", "NUM": 4519, "TIPO1_16": "GA", "TIPO2_16": "GARAGE", "PISOS_16": 1, "NOMBRE": null, "BARRIO": "CHACARITA", "COMUNA": 15.0 }, "geometry": { "type": "Point", "coordinates": [ -58.453724279376523, -34.593995083771482 ] } },
+{ "type": "Feature", "properties": { "SMP": "033-044-012", "CALLE": "BONPLAND", "NUM": 1147, "TIPO1_16": "GA", "TIPO2_16": "GARAGE", "PISOS_16": 1, "NOMBRE": null, "BARRIO": "CHACARITA", "COMUNA": 15.0 }, "geometry": { "type": "Point", "coordinates": [ -58.442584287486973, -34.588191741730228 ] } },
+{ "type": "Feature", "properties": { "SMP": "033-056C-023A", "CALLE": "CORDOBA", "NUM": 5640, "TIPO1_16": "GA", "TIPO2_16": "GARAGE", "PISOS_16": 1, "NOMBRE": null, "BARRIO": "CHACARITA", "COMUNA": 15.0 }, "geometry": { "type": "Point", "coordinates": [ -58.441004627629752, -34.587519112769236 ] } },
+{ "type": "Feature", "properties": { "SMP": "033-012-031", "CALLE": "RAMIREZ DE VELAZCO, JUAN", "NUM": 1418, "TIPO1_16": "GA", "TIPO2_16": "GARAGE", "PISOS_16": 1, "NOMBRE": null, "BARRIO": "CHACARITA", "COMUNA": 15.0 }, "geometry": { "type": "Point", "coordinates": [ -58.444650436120575, -34.590383127964245 ] } },
+{ "type": "Feature", "properties": { "SMP": "033-023-032", "CALLE": "BONPLAND", "NUM": 966, "TIPO1_16": "GA", "TIPO2_16": "GARAGE", "PISOS_16": 1, "NOMBRE": null, "BARRIO": "CHACARITA", "COMUNA": 15.0 }, "geometry": { "type": "Point", "coordinates": [ -58.444840788150081, -34.589018383021205 ] } },
+{ "type": "Feature", "properties": { "SMP": "077-100-023A", "CALLE": "BACACAY", "NUM": 3853, "TIPO1_16": "GA", "TIPO2_16": "GARAGE", "PISOS_16": 1, "NOMBRE": null, "BARRIO": "FLORESTA", "COMUNA": 10.0 }, "geometry": { "type": "Point", "coordinates": [ -58.482299642363472, -34.631132064921729 ] } },
+{ "type": "Feature", "properties": { "SMP": "077-106-002C", "CALLE": "YERBAL", "NUM": 3730, "TIPO1_16": "GA", "TIPO2_16": "GARAGE", "PISOS_16": 1, "NOMBRE": null, "BARRIO": "FLORESTA", "COMUNA": 10.0 }, "geometry": { "type": "Point", "coordinates": [ -58.479736122453012, -34.632507740970219 ] } },
+{ "type": "Feature", "properties": { "SMP": "048-151-017", "CALLE": "BILBAO, FRANCISCO", "NUM": 1935, "TIPO1_16": "GA", "TIPO2_16": "GARAGE", "PISOS_16": 5, "NOMBRE": null, "BARRIO": "FLORES", "COMUNA": 7.0 }, "geometry": { "type": "Point", "coordinates": [ -58.45444118557414, -34.632540504633589 ] } },
+{ "type": "Feature", "properties": { "SMP": "079-069-030A", "CALLE": "BAHIA BLANCA", "NUM": 1625, "TIPO1_16": "GA", "TIPO2_16": "GARAGE", "PISOS_16": 1, "NOMBRE": null, "BARRIO": "FLORESTA", "COMUNA": 10.0 }, "geometry": { "type": "Point", "coordinates": [ -58.491642730487861, -34.618753656157935 ] } },
+{ "type": "Feature", "properties": { "SMP": "079-054-005A", "CALLE": "MAGARINOS CERVANTES, A.", "NUM": 3940, "TIPO1_16": "GA", "TIPO2_16": "GARAGE", "PISOS_16": 2, "NOMBRE": null, "BARRIO": "FLORESTA", "COMUNA": 10.0 }, "geometry": { "type": "Point", "coordinates": [ -58.490680926714823, -34.620784904328239 ] } },
+{ "type": "Feature", "properties": { "SMP": "079-063-016", "CALLE": "BAHIA BLANCA", "NUM": 1051, "TIPO1_16": "GA", "TIPO2_16": "GARAGE", "PISOS_16": 2, "NOMBRE": "MARY - GRA", "BARRIO": "FLORESTA", "COMUNA": 10.0 }, "geometry": { "type": "Point", "coordinates": [ -58.485322272289579, -34.623788688856678 ] } },
+]
+};
---
+++ ProyectoEPT/Pagina/Php/AlquilarCochera.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alquilar Cochera</title>
+</head>
+<body>
+<header>
+
+<h1>Alquilar Cochera</h1>   
+
+<header>
+<form action="./PaginaPrincipal.php">
+<input type="submit" value= "Volver a la pagina principal" name="VolverPrincipal" id="volver" >
+</form>
+</p>
+
+
+
+</body>
+</html>
