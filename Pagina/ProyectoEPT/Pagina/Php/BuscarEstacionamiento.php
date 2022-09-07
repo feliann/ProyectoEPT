6d2c637e0a5320dbb19de2a77dc950b44dbfd46a
lucianojacofsky <101577485+lucianojacofsky@users.noreply.github.com>
2022-09-07 15:08:15 -0300

    Add files via upload

---
+++ Pagina/ProyectoEPT/Pagina/Php/BuscarEstacionamiento.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buscar Estacionamiento</title>
+</head>
+<body>
+<header>
+
+<h1>Buscar estacionamiento</h1>   
+
+<header>
+<form action="./PaginaPrincipal.php">
+<input type="submit" value= "Volver a la pagina principal" name="VolverPrincipal" id="volver" >
+</form>
+</p>
+
+
+
+</body>
+</html>
---
+++ Pagina/ProyectoEPT/Pagina/Php/Login.php
@@ -0,0 +1,36 @@
+<?php
+include ("Conexion.php");
+
+$mail = $_POST["Correo"];
+$contra = $_POST["Contrasena"];
+$nombre = $_POST["Nombre"];
+$apellido = $_POST["Apellido"];
+
+if(isset($_POST["iniciarsesion"]))
+{
+   $query = mysqli_query($conn,"SELECT * FROM registro WHERE Nombre = '$nombre' AND Contrasena = '$contra'");
+   $nr = mysqli_num_rows($query);
+
+   if($nr==1)
+   {
+       echo "<script> alert('Bienvenido: $nombre'); window.location= 'PaginaPrincipal.php' </script>";
+   }
+   else
+   {
+    echo "<script> alert('Usuario inexistente'); window.location= 'iniciosesion.php' </script>";
+   }
+}
+if(isset($_POST["registro"])) 
+{
+    $sqlgrabar = "INSERT INTO registro (Correo,Contrasena,Nombre,Apellido) values ('$mail','$contra','$nombre','$apellido')";
+
+	if(mysqli_query($conn,$sqlgrabar))
+	{
+		echo "<script> alert('Usuario registrado con exito'); window.location='PaginaPrincipal.php' </script>";
+	}else 
+	{
+		echo "Error: ".$sqlgrabar."<br>".mysql_error($conn);
+	}
+} 
+
+
