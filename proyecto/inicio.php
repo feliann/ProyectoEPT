3951f4c264fd17a7476d8551e5bd648727525017
SantinoPaolini <101577619+SantinoPaolini@users.noreply.github.com>
2022-11-03 08:34:40 -0300

    Add files via upload

---
+++ proyecto/inicio.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/master.css">
+  </head>
+  <body style= "background-image: url(estacionar.png)">
+
+    <div class="login-box">
+      <img src="logo.png" class="avatar" alt="Avatar Image">
+      <h1>Iniciar Sesion</h1>
+      <form>
+        <!-- USERNAME INPUT -->
+        <label for="username">Usuario</label>
+        <input type="text" placeholder="Ingresar usuario">
+        <!-- PASSWORD INPUT -->
+        <label for="password">Contraseña</label>
+        <input type="password" placeholder="Ingresar contraeña">
+        <input type="submit" value="Log In">
+        <br>
+        <a href="#">Registrarse</a>
+      </form>
+    </div>
+  </body>
+</html>
